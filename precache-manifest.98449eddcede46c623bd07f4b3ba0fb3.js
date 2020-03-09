38f1542f581180c6dd46fd17c29691752f05cf3c
Auria Aharon <auria54321@gmail.com>
2020-03-09 15:42:02 +0200

    Updates

---
+++ precache-manifest.98449eddcede46c623bd07f4b3ba0fb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e1ae5b24ab508394925d68de0adeea",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "282e60332a05a5fea3f5",
+    "url": "/monsters-rolodex/static/css/main.e2bba9f3.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "282e60332a05a5fea3f5",
+    "url": "/monsters-rolodex/static/js/main.0b23d56b.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
